172784fe61ac820f1999363b9e04c9af6816e730
Hansel <lopezhansel@gmail.com>
2016-03-11 20:56:03 -0700

    Fixing filter problem

--- public/components/topics/topicCtrl.js
+++ public/components/topics/topicCtrl.js
@@ -1,58 +1,39 @@
-
-(function () {
+(function() {
     'use strict';
 
-    
+
     //$routeParams comes with ngRoute. We use it to create dynamic paths by including ":routename" in our routeprovider.
-    app.controller("topicsCtrl", ["$scope","$routeParams", '$http', function($scope,$routeParams, $http){
+    app.controller("topicsCtrl", ["$scope", "$routeParams", '$http', function($scope, $routeParams, $http) {
         $scope.resources = [];
         $scope.topics = [];
 
         $scope.url = 'https://spreadsheets.google.com/feeds/list/1mjePr-P66WAHm0LINEuquvSPhOtfkCf6vWaqLJK09VI/default/public/values?alt=json';
         $http.get($scope.url)
-        .then(function(res){
-            $scope.topics = res.data.feed.entry;
+            .then(function(res) {
+                $scope.topics = res.data.feed.entry;
 
-             //here i will go through each topic and find where
-            $scope.feature = $scope.topics.filter(function (el) {
-                // the link matches my routeParams.topicsTitle
-                 if (el.gsx$link.$t === $routeParams.topicTitle) {
-                    // then set $scope.feature to that one El
-                    return el;
-                 }
-            })
+                //here i will go through each topic and find where
+                $scope.feature = $scope.topics.filter(function(el) {
+                    // the link matches my routeParams.topicsTitle
+                    if (el.gsx$link.$t === $routeParams.topicTitle) {
+                        // then set $scope.feature to that one El
+                        return el;
+                    }
+                })
 
 
 
-        });
-
-        // $scope.url = 'https://spreadsheets.google.com/feeds/list/1VRch1vAe9vrZytWTcm_JboMLdXx26os10nLBEjKrUUE/default/public/values?alt=json';
-        // $http.get($scope.url)
-        // .then(function(res){
-        //     var entries = res.data.feed.entry;
-        //     //console.log(res.data);
-
-        //     for(var item in entries) {
-        //        //console.log(entries[item].gsx$topic.$t, $routeParams.topicTitle);
-        //        //  console.log(entries[item]);
-
-        //         if(entries[item].gsx$topic.$t === $routeParams.topicTitle){
-        //             $scope.resources.push(entries[item]);
-        //         };
-        //     };
-
-        //     // console.log(res.data, res);
-        // });
+            });
 
         $scope.url = 'https://spreadsheets.google.com/feeds/list/1VRch1vAe9vrZytWTcm_JboMLdXx26os10nLBEjKrUUE/default/public/values?alt=json';
         $http.get($scope.url)
-            .then(function(res){
+            .then(function(res) {
                 var entries = res.data.feed.entry;
-                $scope.resources = entries.filter(function (el,inn) {
-                    if(entries[inn].gsx$topic.$t === $routeParams.topicTitle){
+                $scope.resources = entries.filter(function(el, inn) {
+                    if (entries[inn].gsx$topic.$t === $routeParams.topicTitle) {
                         return true;
                     }
-                }).map(function (el,inn) {
+                }).map(function(el, inn) {
                     var out = {};
                     out.comments = el.gsx$comments.$t;
                     out.description = el.gsx$description.$t;
@@ -69,7 +50,6 @@
                     return out;
                 });
 
-console.log($scope.resources);
             });
 
 
@@ -77,24 +57,20 @@ console.log($scope.resources);
         $scope.gradeFilter = '';
         $scope.searchFilter = '';
 
-        $scope.filterResources = function (grade, media){
+        $scope.filterResources = function(grade, media) {
             $scope.gradeFilter = grade;
             $scope.mediaFilter = media;
-            
-        };//how can we implement filtering for resources that may have more than one grade assigned to them?,
+
+        }; //how can we implement filtering for resources that may have more than one grade assigned to them?,
         //let's set up one view for an actual subtopic
 
         // $scope.topic = topics[Suppy]
-        
+
         $scope.topicTitle = $routeParams.topicTitle;
 
- 
-        
-    }]);
-    
-    
-    
 
+
+    }]);
 
 
 
