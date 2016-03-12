c54c2ba8776e887da694a94b634a06f7214671b6
Hansel <lopezhansel@gmail.com>
2016-03-12 00:58:52 -0700

    Simplified code

--- public/components/topics/topicCtrl.js
+++ public/components/topics/topicCtrl.js
@@ -1,54 +1,50 @@
 (function() {
     'use strict';
-    //$routeParams comes with ngRoute. We use it to create dynamic paths by including ":routename" in our routeprovider.
+    
     app.controller("topicsCtrl", ["$scope", "$routeParams", '$http', function($scope, $routeParams, $http) {
-        $scope.resources = [];
-        $scope.topics = [];
-        $scope.topicUrl = 'https://spreadsheets.google.com/feeds/list/1mjePr-P66WAHm0LINEuquvSPhOtfkCf6vWaqLJK09VI/default/public/values?alt=json';
-        $http.get($scope.topicUrl).then(function(res) {
-            $scope.topics = res.data.feed.entry;
-            //here i will go through each topic and find where
-            $scope.feature = $scope.topics.filter(function(el) {
-                // the link matches my routeParams.topicsTitle
-                if (el.gsx$link.$t === $routeParams.topicTitle) {
-                    // then set $scope.feature to that one El
-                    return el;
-                }
-            });
-        });
-        $scope.resourcesUrl = 'https://spreadsheets.google.com/feeds/list/1VRch1vAe9vrZytWTcm_JboMLdXx26os10nLBEjKrUUE/default/public/values?alt=json';
-        $http.get($scope.resourcesUrl).then(function(res) {
-            var entries = res.data.feed.entry;
-            $scope.resources = entries.filter(function(el, inn) {
-                if (entries[inn].gsx$topic.$t === $routeParams.topicTitle) {
-                    return true;
-                }
-            }).map(function(el, inn) {
-                var out = {};
-                out.comments = el.gsx$comments.$t;
-                out.description = el.gsx$description.$t;
-                out.featured = el.gsx$featured.$t;
-                out.grade = el.gsx$grade.$t;
-                out.icon = el.gsx$icon.$t;
-                out.link = el.gsx$link.$t;
-                out.rating = el.gsx$rating.$t;
-                out.source = el.gsx$source.$t;
-                out.subject = el.gsx$subject.$t;
-                out.title = el.gsx$title.$t;
-                out.topic = el.gsx$topic.$t;
-                out.type = el.gsx$type.$t;
-                return out;
-            });
-        });
+        var topicsSheetUrl = 'https://spreadsheets.google.com/feeds/list/1mjePr-P66WAHm0LINEuquvSPhOtfkCf6vWaqLJK09VI/default/public/values?alt=json';
+        var resourceSheetUrl = 'https://spreadsheets.google.com/feeds/list/1VRch1vAe9vrZytWTcm_JboMLdXx26os10nLBEjKrUUE/default/public/values?alt=json';
         $scope.mediaFilter = '';
         $scope.gradeFilter = '';
         $scope.searchFilter = '';
-        $scope.filterResources = function(grade, media) {
+        $scope.resources = [];
+        $scope.topicsSheet = [];
+        $scope.currentTopic = $routeParams.currentTopic; 
+        $scope.filterResources = filterResources;
+
+        $http.get(topicsSheetUrl).then(function(res) {
+            $scope.topicsSheet = res.data.feed.entry;
+            $scope.featuredTopic = $scope.topicsSheet.map(removeGsx$).filter(matchesCurentPage)[0];
+        });
+
+        $http.get(resourceSheetUrl).then(function(res) {
+            var sheet = res.data.feed.entry;
+            $scope.resources = sheet.map(removeGsx$).filter(matchesCurentPage);
+        });
+
+        function matchesCurentPage (topicObj){
+            return (topicObj.page === $routeParams.currentTopic)? true : false;
+        }
+        function removeGsx$ (resourceObj) { 
+            var newObj = {};
+            for(var prop in resourceObj ){
+                if (prop.substring(0,4) === "gsx$"){
+                    var str = prop.substring(4,prop.length);
+                    newObj[str] = resourceObj[prop].$t;
+                }
+            }
+            return newObj;
+        }
+        function deleteProps (inputObj,str1,str2,str3) {
+            for (var ii = 1; ii < arguments.length; ii++) {
+                delete inputObj[arguments[ii]];
+            }
+            return inputObj;
+        }
+        function filterResources (grade, media) {
             $scope.gradeFilter = grade;
             $scope.mediaFilter = media;
-        }; //how can we implement filtering for resources that may have more than one grade assigned to them?,
-        //let's set up one view for an actual subtopic
-        // $scope.topic = topics[Suppy]
-        $scope.topicTitle = $routeParams.topicTitle;
+        } 
+
     }]);
 })();
